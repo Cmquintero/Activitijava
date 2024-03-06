151938787f6614bc3c7785e114d4901fedd030ba
cmquintero <cmquinterot@ufpso.edu.co>
2024-03-06 15:16:40 -0500

    first commit

---
+++ src/main/java/com/mycompany/activitijava/Activitijava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.activitijava;
+
+/**
+ *
+ * @author SCIS2PC15
+ */
+public class Activitijava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
